8443180b922c3257b9c3ecb0bcee3b3a8100f8f7
Devin Rahul <devinrahul96@gmail.com>
2023-10-18 22:30:46 +1100

    Header & Date Picker

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css'
+import Banner from '../components/Banner'
+
+function Home() {
+  return (
+    <div className='home'>
+      <Banner/>
+      <div className='home__section'>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+      <div className='home__section'>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
